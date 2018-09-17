f99ea6f984b6451447568da2d650308141aedd28
Nat Lasseter <Nat Lasseter nathan@bytemark.co.uk>
2018-09-17 15:54:20 +0100

    Added targeting reticule, error highlighting, and right/center click.

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ let
   cols = 15,
   scale = 25,
   grid = [],
-  windiv;
+  windiv,
+  reticule = false,
+  errors = false;
 
 function setup() {
 	createCanvas(scale*cols, scale*rows);
@@ -18,35 +20,55 @@ function setup() {
   }
 
   textAlign(CENTER, CENTER);
-  noStroke();
   textSize(14);
 
   windiv = createP('');
 }
 
 function draw() {
+  noStroke();
   for(let row = 0; row < rows; row++) {
     for(let col = 0; col < cols; col++) {
       switch(grid[row][col]['clicked']) {
         case 1:
           fill(255);
           rect(col*scale, row*scale, scale, scale);
-          fill(0);
+          if(errors && error(row, col))
+            fill(255, 0, 0);
+          else
+            fill(0);
           text(nsum(row, col), (col+0.5)*scale, (row+0.5)*scale);
           break;
         case 2:
           fill(0);
           rect(col*scale, row*scale, scale, scale);
+          if(errors && error(row, col))
+            fill(255, 0, 0);
+          else
           fill(255);
           text(nsum(row, col), (col+0.5)*scale, (row+0.5)*scale);
           break;
         default:
           fill(159);
           rect(col*scale, row*scale, scale, scale);
+          if(errors && error(row, col))
+            fill(255, 0, 0);
+          else
           fill(0);
           text(nsum(row, col), (col+0.5)*scale, (row+0.5)*scale);
       }
     }
+  }
+
+  if(reticule) {
+    let
+      col = Math.floor(mouseX / scale),
+    row = Math.floor(mouseY / scale);
+    noFill();
+    strokeWeight(1);
+    stroke(255, 0, 0);
+    rect((col-1)*scale, (row-1)*scale, 3*scale, 3*scale);
+    rect(col*scale, row*scale, scale, scale);
   }
 
   let correct = 0;
@@ -70,8 +92,33 @@ function mousePressed() {
     col = Math.floor(mouseX / scale),
     row = Math.floor(mouseY / scale);
 
-  grid[row][col]['clicked'] += 1;
-  grid[row][col]['clicked'] %= 3;
+  switch(mouseButton) {
+    case LEFT:
+      grid[row][col]['clicked'] += 1;
+      break;
+    case RIGHT:
+      grid[row][col]['clicked'] -= 1;
+      break;
+    default:
+      grid[row][col]['clicked'] = 0;
+  }
+
+  if(grid[row][col]['clicked'] == 3)
+    grid[row][col]['clicked'] = 0;
+  if(grid[row][col]['clicked'] == -1)
+    grid[row][col]['clicked'] = 2;
+
+  return false;
+}
+
+function keyPressed() {
+  switch(key) {
+    case 'R':
+      reticule = !reticule;
+      break;
+    case 'E':
+      errors = !errors;
+  }
 }
 
 function nsum(r, c) {
@@ -89,4 +136,29 @@ function nsum(r, c) {
   if(r<(rows-1) && c<(cols-1) && grid[r+1][c+1]['corrupt']) sum += 1;
 
   return sum;
+}
+
+function error(r, c) {
+  if( (r>0        && c>0        && grid[r-1][c-1]['clicked'] == 0 ) ||
+      (r>0                      && grid[r-1][c]['clicked']   == 0 ) ||
+      (r>0        && c<(cols-1) && grid[r-1][c+1]['clicked'] == 0 ) ||
+      (              c>0        && grid[r][c-1]['clicked']   == 0 ) ||
+      (              c<(cols-1) && grid[r][c+1]['clicked']   == 0 ) ||
+      (r<(rows-1) && c>0        && grid[r+1][c-1]['clicked'] == 0 ) ||
+      (r<(rows-1)               && grid[r+1][c]['clicked']   == 0 ) ||
+      (r<(rows-1) && c<(cols-1) && grid[r+1][c+1]['clicked'] == 0 ) )
+    return false;
+
+  let sum = 0;
+
+  if(r>0 && c>0 && grid[r-1][c-1]['clicked'] == 1) sum += 1;
+  if(r>0 && grid[r-1][c]['clicked'] == 1) sum += 1;
+  if(r>0 && c<(cols-1) && grid[r-1][c+1]['clicked'] == 1) sum += 1;
+  if(c>0 && grid[r][c-1]['clicked'] == 1) sum += 1;
+  if(c<(cols-1) && grid[r][c+1]['clicked'] == 1) sum += 1;
+  if(r<(rows-1) && c>0 && grid[r+1][c-1]['clicked'] == 1) sum += 1;
+  if(r<(rows-1) && grid[r+1][c]['clicked'] == 1) sum += 1;
+  if(r<(rows-1) && c<(cols-1) && grid[r+1][c+1]['clicked'] == 1) sum += 1;
+
+  return nsum(r, c) != sum;
 }
